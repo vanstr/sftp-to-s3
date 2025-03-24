b14c1b39189c286cea12169ff4a0e306b86b4e58
Ivan Mihnenok <mihnneok@gmail.com>
2025-03-24 11:45:38 +0200

    init project

---
+++ src/main/java/dev/foryour/sftp_for_s3/SftpForS3Application.java
@@ -0,0 +1,13 @@
+package dev.foryour.sftp_for_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpForS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpForS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/foryour/sftp_for_s3/SftpForS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.foryour.sftp_for_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftpForS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
